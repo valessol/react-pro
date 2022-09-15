1e2c51d6bb1ae1f8ddb529689345e38c96701fbe
valeria <vsol.sil@gmail.com>
2022-09-15 17:38:03 -0300

    01-bases first commit

---
+++ src/bases/Counter.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+const Counter = ({initialValue = 0}) => {
+  const [counter, setCounter] = useState(initialValue);
+  const handleClick = () => {
+    setCounter(prev => prev + 1);
+  }
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={handleClick}>+1</button>
+    </>
+  )
+}
+
+export default Counter
