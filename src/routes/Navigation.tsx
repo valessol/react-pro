719fc9330bf7ec8fd71494b3d6b6e120c5388aef
Valeria Silveira <vsol.sil@gmail.com>
2022-10-11 09:08:04 -0300

    adding components to routes

---
+++ src/routes/Navigation.tsx
@@ -0,0 +1,41 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  NavLink, Navigate,
+} from 'react-router-dom';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../lazyload/pages';
+
+import logo from '../logo.svg';
+
+export const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <div className="main-layout">
+        <nav>
+            <img src={ logo } alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/lazy1" className={({isActive}) => isActive ? "nav-active": ""} >Lazy 1</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy2" className={({isActive}) => isActive ? "nav-active": ""}>Lazy 2</NavLink>
+            </li>
+            <li>
+              <NavLink to="/lazy3" className={({isActive}) => isActive ? "nav-active": ""}>Lazy 3</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/lazy1" element={<LazyPage1 />}/>
+          <Route path="/lazy2" element={<LazyPage2 />}/>
+          <Route path="/lazy3" element={<LazyPage3 />}/>
+          <Route path="/*" element={<Navigate to="/lazy1" replace />}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
