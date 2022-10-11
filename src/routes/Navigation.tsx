13372d386c2e1c922382d0807356599392387331
Valeria Silveira <vsol.sil@gmail.com>
2022-10-11 09:33:03 -0300

    38-adding key to map function

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -15,17 +15,15 @@ export const Navigation = () => {
             <img src={ logo } alt="React Logo" />
           <ul>
             { routes.map(({to, name}) =>
-              <li>
+              <li key={to}>
                 <NavLink to={to} className={({isActive}) => isActive ? "nav-active": ""} >{name}</NavLink>
               </li>
             )}
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
         <Routes>
-          { routes.map(({path, Component}) => <Route path={path} element={<Component />}/>)}
+          { routes.map(({path, Component}) => <Route key={path} path={path} element={<Component />}/>)}
           <Route path="/*" element={<Navigate to={routes[0].to} replace />}/>
         </Routes>
       </div>
