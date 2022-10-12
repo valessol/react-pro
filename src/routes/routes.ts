7c05f002a97d247af025b05060f44fcbcc2c241c
Valeria Silveira <vsol.sil@gmail.com>
2022-10-12 09:06:13 -0300

    40-change chunks name

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -8,9 +8,9 @@ interface Route {
     name: string;
 }
 
-const Lazy1 = lazy(() => import('../lazyload/pages/LazyPage1'))
-const Lazy2 = lazy(() => import('../lazyload/pages/LazyPage2'))
-const Lazy3 = lazy(() => import('../lazyload/pages/LazyPage3'))
+const Lazy1 = lazy(() => import(/* webpackChunkName: "LazyPage1" */'../lazyload/pages/LazyPage1'))
+const Lazy2 = lazy(() => import(/* webpackChunkName: "LazyPage2" */'../lazyload/pages/LazyPage2'))
+const Lazy3 = lazy(() => import(/* webpackChunkName: "LazyPage3" */'../lazyload/pages/LazyPage3'))
 
 export const routes: Route[] = [
     {
