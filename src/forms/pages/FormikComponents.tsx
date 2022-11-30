b4bc6d4f359be11a4cfefdfdf461b3b337e19bfc
Valeria Silveira <ssilveira@nosis.com>
2022-11-30 13:16:07 -0300

    add checkbox and select

--- src/forms/pages/FormikComponents.tsx
+++ src/forms/pages/FormikComponents.tsx
@@ -10,6 +10,8 @@ const FormikComponents = () => {
           firstName: "",
           lastName: "",
           email: "",
+          terms: false,
+          jobType: "",
         }}
         onSubmit={(values) => {
           console.log(values);
@@ -22,6 +24,13 @@ const FormikComponents = () => {
             .max(10, "Debe tener 10 caracteres o menos")
             .required("Requerido"),
           email: Yup.string().required("Requerido").email("Formato inválido"),
+          terms: Yup.boolean().oneOf(
+            [true],
+            "Los terminos y condiciones son obligatorios"
+          ),
+          jobType: Yup.string()
+            .notOneOf(["it-jr"], "Esta opción no es permitida")
+            .required("Requerido"),
         })}
       >
         {(formik) => (
@@ -37,6 +46,22 @@ const FormikComponents = () => {
             <label htmlFor="email">Email Address</label>
             <Field name="email" type="text" />
             <ErrorMessage name="email" component="span" />
+
+            <label htmlFor="jobType">Job Type</label>
+            <Field name="jobType" as="selects">
+              <option value="">Pick something</option>
+              <option value="developer">Developer</option>
+              <option value="designer">Designer</option>
+              <option value="it-senior">IT Senior</option>
+              <option value="it-jr">IT Jr.</option>
+            </Field>
+            <ErrorMessage name="jobType" component="span" />
+
+            <label>
+              <Field name="terms" type="checkbox" />
+              Terms and conditions
+            </label>
+            <ErrorMessage name="terms" component="span" />
 
             <button type="submit">Submit</button>
           </Form>
