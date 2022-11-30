eff00cfa391eae55dcb6bde5d2b85faa36a5a7c1
Valeria Silveira <ssilveira@nosis.com>
2022-11-30 09:53:19 -0300

    basic form

---
+++ src/forms/hooks/useForm.ts
@@ -0,0 +1,30 @@
+import { useState, ChangeEvent } from "react";
+
+export const useForm = <T>(initialData: T) => {
+  const [formData, setFormData] = useState(initialData);
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const resetForm = () => {
+    setFormData({ ...initialData });
+  };
+
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+
+  return {
+    ...formData,
+
+    isValidEmail,
+    onChange,
+    resetForm,
+  };
+};
---
+++ src/forms/pages/RegisterPage.tsx
@@ -0,0 +1,80 @@
+import { FormEvent } from "react";
+import { useForm } from "../hooks/useForm";
+import "../styles/styles.css";
+
+const RegisterPage = () => {
+  const {
+    name,
+    email,
+    password1,
+    password2,
+    isValidEmail,
+    onChange,
+    resetForm,
+  } = useForm({
+    name: "",
+    email: "",
+    password1: "",
+    password2: "",
+  });
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  console.log(name.trim().length);
+  return (
+    <div>
+      <h1>Register Page</h1>
+
+      <form action="" onSubmit={onSubmit} noValidate>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          name="name"
+          onChange={onChange}
+          className={`${!name.trim().length && "has-error"}`}
+        />
+        {!name.trim().length && <span>Este campo es obligatorio</span>}
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          name="email"
+          onChange={onChange}
+          className={`${!isValidEmail(email) && "has-error"}`}
+        />
+        {!isValidEmail(email) && <span>El mail ingresado es inválido</span>}
+        <input
+          type="password"
+          placeholder="Password"
+          value={password1}
+          name="password1"
+          onChange={onChange}
+        />
+        {!password1.trim().length && <span>Este campo es obligatorio</span>}
+        {password1.trim().length && password1.trim().length < 6 ? (
+          <span>Debe contener al menos 6 caracteres</span>
+        ) : null}
+        <input
+          type="password"
+          placeholder="Repeat Password"
+          value={password2}
+          name="password2"
+          onChange={onChange}
+        />
+        {!password2.trim().length && <span>Este campo es obligatorio</span>}
+        {password2.trim().length && password2 !== password1 ? (
+          <span>Las contraseñas no coinciden</span>
+        ) : null}
+
+        <button type="submit">Create</button>
+        <button type="button" onClick={resetForm}>
+          Reset Form
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterPage;
